e565c805f3ee35883162237e55e8b48b4cfc42b6
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-10-18 04:55:06 +0900

    fix indent

--- model/book.py
+++ model/book.py
@@ -2,13 +2,13 @@
 from google.appengine.ext import db
 
 class Book(db.Model):
-    isbn = db.StringProperty()
-    title = db.StringProperty()
-    author = db.StringProperty()
-    publisher = db.StringProperty()
+    isbn         = db.StringProperty()
+    title        = db.StringProperty()
+    author       = db.StringProperty()
+    publisher    = db.StringProperty()
     published_at = db.DateProperty()
-    created_at = db.DateTimeProperty(auto_now_add=True)
+    created_at   = db.DateTimeProperty(auto_now_add = True)
 
-  def path(self):
-    return "/book/" + self.isbn
+    def path(self):
+        return "/book/" + self.isbn
 
