51307ffe2b8616cc8067b8ab3db092b628c6a946
hitode909 <hitode909@gmail.com>
2009-10-18 03:22:15 +0900

    indent 4

--- model/book.py
+++ model/book.py
@@ -2,12 +2,12 @@
 from google.appengine.ext import db
 
 class Book(db.Model):
-  isbn = db.StringProperty()
-  title = db.StringProperty()
-  author = db.StringProperty()
-  publisher = db.StringProperty()
-  published_at = db.DateProperty()
-  created_at = db.DateTimeProperty(auto_now_add=True)
+    isbn = db.StringProperty()
+    title = db.StringProperty()
+    author = db.StringProperty()
+    publisher = db.StringProperty()
+    published_at = db.DateProperty()
+    created_at = db.DateTimeProperty(auto_now_add=True)
 
   def path(self):
     return "/book/" + self.isbn
