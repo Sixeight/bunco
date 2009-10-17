334784d7c833ef7478824d5c56241996b2787b3d
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-10-18 02:03:37 +0900

    change indent size

--- model/book.py
+++ model/book.py
@@ -2,10 +2,10 @@
 from google.appengine.ext import db
 
 class Book(db.Model):
-  asin = db.StringProperty()
-  title = db.StringProperty()
-  author = db.StringProperty()
-  publisher = db.StringProperty()
-  published_at = db.DateProperty()
-  created_at = db.DateTimeProperty(auto_now_add=True)
+    asin = db.StringProperty()
+    title = db.StringProperty()
+    author = db.StringProperty()
+    publisher = db.StringProperty()
+    published_at = db.DateProperty()
+    created_at = db.DateTimeProperty(auto_now_add=True)
 
