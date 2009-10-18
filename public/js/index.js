88e7b07074fd2ed3287bb677a3629ffb312c2f2d
hitode909 <hitode909@gmail.com>
2009-10-18 12:16:17 +0900

    support hatenastar

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+Hatena.Star.Token = 'dfe73460b07ea3bf681104e615d5ecd85df02e2e';
+Hatena.Star.SiteConfig = {
+    entryNodes: {
+        'tr.book': {
+            uri: 'span.title a',
+            title: 'span.title a',
+            container: 'span.title'
+        }
+    }
+};
