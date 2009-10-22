6ce74220869b478535523966b8e65dee806cf605
hitode909 <hitode909@gmail.com>
2009-10-22 09:49:20 +0900

    トップページのスター対応

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,29 @@
 Hatena.Star.Token = 'dfe73460b07ea3bf681104e615d5ecd85df02e2e';
 Hatena.Star.SiteConfig = {
     entryNodes: {
-        'tr.book': {
-            uri: 'span.title a',
-            title: 'span.title a',
-            container: 'span.title'
+        '.book': {
+            uri: '.label a',
+            title: '.about .title',
+            container: '.about .title'
         }
     }
+};
+
+Hatena.Star.EntryLoader.loadEntries = function(node) {
+    var c = Hatena.Star.EntryLoader;
+    var entries = c.loadEntriesByConfig(node);
+    
+    $(".activity", node).each(function() {
+        var e = {entryNode: this};
+        e.title = $(".title", this).text();
+        e.uri = "http://hacobunko.appspot.com/activity/" + $(".key", this).text();
+        e.comment_container = c.createCommentContainer();
+        $(".title", this).append(e.comment_container);
+        e.star_container = c.createStarContainer();
+        $(".title", this).append(e.star_container);
+        entries.push(e);
+    });
+    return entries;
 };
 
 
